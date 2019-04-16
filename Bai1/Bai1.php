b9d5126b4cc3a518c9cc7693750bda7440759ab0
Thuy0321 <thuy97it@gmail.com>
2019-04-16 09:13:31 +0700

    Bai1

--- Bai1/Bai1.php
+++ Bai1/Bai1.php
@@ -7,11 +7,11 @@
 <body>
 	<h3>Giải phương trình bậc hai</h3>
     <form method="post" action="">
-        <input type="text" style="width: 30px" name="a" autofocus pattern="[0-9]?[0-9]" value="<?php echo isset($_POST['a']) ? $_POST['a'] : '' ?>"/>x<sup>2</sup>
+        <input type="text" style="width: 30px" name="a" autofocus value="<?php echo isset($_POST['a']) ? $_POST['a'] : '' ?>"/>x<sup>2</sup>
         +
-        <input type="text" style="width: 30px" name="b" autofocus pattern="[0-9]?[0-9]" value="<?php echo isset($_POST['b']) ? $_POST['b'] : '' ?>"/>x
+        <input type="text" style="width: 30px" name="b" autofocus value="<?php echo isset($_POST['b']) ? $_POST['b'] : '' ?>"/>x
         + 
-        <input type="text" style="width: 30px" name="c" autofocus pattern="[0-9]?[0-9]" value="<?php echo isset($_POST['c']) ? $_POST['c'] : '' ?>"/>
+        <input type="text" style="width: 30px" name="c" autofocus value="<?php echo isset($_POST['c']) ? $_POST['c'] : '' ?>"/>
         = 0
         <br/><br/>
         <input type="submit" name="gptb2" value="Tính" />
@@ -23,13 +23,14 @@
 		$a=isset($_POST['a']) ? $_POST['a']:'';
 		$b=isset($_POST['b']) ? $_POST['b']:'';
 		$c=isset($_POST['c']) ? $_POST['c']:'';
-		if(empty($a)||empty($b)||empty($c))
+		if($a=='0' && $b=='0' && $c=='0')
+			$result="Phương trình đúng với mọi nghiệm.";
+		else if(empty($a)||empty($b)||empty($c))
 			echo"Bạn chưa nhập đủ dữ liệu.";
-		else{
+		else if(is_numeric($a)&&is_numeric($b)&&is_numeric($c))
+		{
 			$delta=($b*$b) - (4*$a*$c);
-			if($a==0 && $b==0 && $c==0)
-				$result="Phương trình có vô số nghiệm.";
-			else if($a==0 && $b==0)
+			if($a==0 && $b==0)
 				$result="Phương trình vô nghiệm.";
 			else if($a==0)
 				$result="Phương trình có nghiệm duy nhất: x=".(-$c/$b);
@@ -41,6 +42,8 @@
 				$result="Phương trình có 2 nghiệm phân biệt: x1= ".((-$b+sqrt($delta))/(2*$a)).", x2= ".((-$b-sqrt($delta))/(2*$a));
 			}
 		}
+		else
+			echo "Sai định dạng";
 	}
     echo $result; 
     ?>
