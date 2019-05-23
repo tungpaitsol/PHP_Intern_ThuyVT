3e2b3a1e171eed18e34a91bb956217d988b4dbda
Thuy0321 <thuy97it@gmail.com>
2019-05-23 11:37:37 +0700

    feature/OOP_lession_1 Update

--- OOP_Bai1/OOP_Bai1.php
+++ OOP_Bai1/OOP_Bai1.php
@@ -120,6 +120,35 @@ class WorkTime{
 	public function getEndDatetime(){
 		return $this->end_datetime;
 	}
+	public function CheckMonth($m,$y){
+		switch ($m) {
+			case 1: 
+			case 3: 
+			case 5: 
+			case 7: 
+			case 8: 
+			case 10: 
+			case 12:
+				$d=31;
+				break; 
+			case 4: 
+			case 6: 
+			case 9: 
+			case 11: 
+				$d=30;
+				break; 
+			case 2: 
+				if( $y % 100 != 0 && $y % 4 == 0 ) { 
+					$d=29;
+				} else { 
+					$d=28;
+				} 
+				break; 
+			default:
+				$d=0;
+		}
+		echo "Tháng ".$m." năm ".$y." có ".$d." ngày";
+	}
 }
 
 $employeefulltime = array();
@@ -146,7 +175,7 @@ function tinhcong($employee,$worktime){
 		$count=0;
 		foreach ($worktime as $value) {
 			if($value->getMemberCode()==$val->getMemberCode()){
-				if((strtotime($value->getStartDatetime()) <= strtotime($val->getStartWorkTime()))
+				if((date("H:i:s",strtotime($value->getStartDatetime())) <= strtotime($val->getStartWorkTime()))
 					&& (date("H:i:s",strtotime($value->getEndDatetime())) >= date("H:i:s",strtotime($val->getStartWorkTime()))))
 				{
 					if($val->getHasLunchBreak()=="1"){
