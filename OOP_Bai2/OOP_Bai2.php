5f20b3b5cc7a01f2864b52e93a4369742b3d7287
Thuy0321 <thuy97it@gmail.com>
2019-06-14 15:03:59 +0700

    OOP_lession_2 Update

--- OOP_Bai2/OOP_Bai2.php
+++ OOP_Bai2/OOP_Bai2.php
@@ -2,33 +2,6 @@
 if (isset($_GET['lang'])) {
 	$_SESSION['lang']= $_GET['lang'];
 }
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Test bài 2 OOP</title>
-	<link rel="stylesheet" href="">
-</head>
-<body>
-	<?php echo $_SESSION['lang']=='en' ? "Select language: " : "Lựa chọn ngôn ngữ: " ?>
-	<br>
-	<form method='get' action=''>
-		<select name='lang' onchange="handleSelect(this)" >
-			<option value='en' <?php if(isset($_SESSION['lang'])&&$_SESSION['lang']=='en') echo "selected" ?> >English</option>
-			<option value='vi' <?php if(isset($_SESSION['lang'])&&$_SESSION['lang']=='vi') echo "selected" ?> >Tiếng Việt</option>
-		</select>
-	</form>
-</body>
-</html>
-<script type="text/javascript">
-	function handleSelect(elm)
-	{
-		window.location.href = "OOP_Bai2.php?lang="+elm.value;
-	}
-</script>
-<?php 
 class Language
 {
 	private $name;
@@ -63,6 +36,7 @@ class LangContext
 		//Tạo mảng sử dụng các giá trị từ mảng $key là các key và các giá trị từ mảng $value là các value tương ứng:
 		$arr=array_combine($key,$value); 
 		return $arr;
+		fclose($myfile);
 	}
 }
 $language = array(
@@ -71,7 +45,46 @@ $language = array(
 );
 $lang=$_SESSION['lang'];
 $content = $language[$lang]->getValue();
-foreach ($content as $value) {
-	echo "<pre>".$value;
-}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Bài 2 OOP</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="custom.css">
+</head>
+<body>
+	<?php echo $_SESSION['lang']=='en' ? "Select language: " : "Lựa chọn ngôn ngữ: " ?>
+	<br>
+	<form class="form_lang" method='get' action=''>
+		<select name='lang' onchange="handleSelect(this)" >
+			<option value='en' <?php if(isset($_SESSION['lang'])&&$_SESSION['lang']=='en') echo "selected" ?> >English</option>
+			<option value='vi' <?php if(isset($_SESSION['lang'])&&$_SESSION['lang']=='vi') echo "selected" ?> >Tiếng Việt</option>
+		</select>
+	</form>
+	<form class="form_reg" method="post">
+		<div class="container">
+			<h1><?php echo $_SESSION['lang']=='en' ? "REGISTER" : "ĐĂNG KÝ" ?></h1>
+			<p><?php echo $_SESSION['lang']=='en' ? "Creat your account. It's free and only takes a minute. " : "Tạo tài khoản của bạn. Miễn phí và chỉ mất một phút. " ?></p>
+			<div class="name">
+				<input class="col-md-6 cl1" type="text" placeholder="<?php echo isset($_SESSION['lang'])?$content['firstname']:"" ?>"name="firstname">
+				<input class="col-md-6 cl2" type="text" placeholder="<?php echo isset($_SESSION['lang'])?$content['lastname']:"" ?>" name="lastname">
+			</div>
+			<input class="col-md-12" type="text" placeholder="<?php echo isset($_SESSION['lang'])?$content['email']:"" ?>" name="email">
+			<input class="col-md-12" type="password" placeholder="<?php echo isset($_SESSION['lang'])?$content['password']:"" ?>" name="password">
+			<input class="col-md-12" type="password" placeholder="<?php echo isset($_SESSION['lang'])?$content['confirmpassword']:"" ?>" name="confirm">
+			<input type="submit" value="<?php echo isset($_SESSION['lang'])?$content['registernow']:"" ?>" class="registerbtn">
+		</div>
+	</form>
+</body>
+</html>
+<script type="text/javascript">
+	function handleSelect(elm)
+	{
+		window.location.href = "test1.php?lang="+elm.value;
+	}
+</script>
